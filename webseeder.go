3515a50c68afdf7b1caa3e87ceff990bd629f7d1
zvodd <zv.odd.101@gmail.com>
2020-03-02 18:11:29 +0800

    remember to always use gofmt

--- webseeder.go
+++ webseeder.go
@@ -15,16 +15,16 @@ import (
 	"github.com/spf13/viper"
 )
 
-func init(){
-	viper.SetConfigName("webseeder.cfg") // name of config file (without extension)
-	viper.SetConfigType("properties") // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath("$HOME/.webseeder")  // call multiple times to add many search paths
-	viper.AddConfigPath(".")               // optionally look for config in the working directory
-	err := viper.ReadInConfig() // Find and read the config file
-	if err != nil { // Handle errors reading the config file
+func init() {
+	viper.SetConfigName("webseeder.cfg")    // name of config file (without extension)
+	viper.SetConfigType("properties")       // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("$HOME/.webseeder") // call multiple times to add many search paths
+	viper.AddConfigPath(".")                // optionally look for config in the working directory
+	err := viper.ReadInConfig()             // Find and read the config file
+	if err != nil {                         // Handle errors reading the config file
 		log.Fatalf("Fatal error config file: %s \n", err)
 	}
-	
+
 	if err := validateConfig(); err != nil {
 		log.Fatalf("Fatal configuration error: %s \n", multierror.Flatten(err))
 	}
@@ -32,36 +32,37 @@ func init(){
 
 var (
 	mainPort string
-	useTLS = false
-	tlsPort string
-	tlsCert string
-	tlsKey string
+	useTLS   = false
+	tlsPort  string
+	tlsCert  string
+	tlsKey   string
 
 	htAuthUser string
 	htAuthPass string
 
-	staticFilePath string
+	staticFilePath   string
 	torrentsFilePath string
-
 )
 
-func validateConfig() error{
+func validateConfig() error {
 	var result *multierror.Error
 	mainPort = viper.GetString("port")
-	portint, err := strconv.Atoi(mainPort);
-	if err != nil {
-		repErr := multierror.Prefix(err, "Port not valid number") 
-		result = multierror.Append(result, repErr)
-	}
-	if portint <= 0 || portint > 65535 {
-		result = multierror.Append(result, errors.New("Port outside valid range"))
+	if len(mainPort > 0) {
+		portint, err := strconv.Atoi(mainPort)
+		if err != nil {
+			repErr := multierror.Prefix(err, "Port not valid number")
+			result = multierror.Append(result, repErr)
+		}
+		if portint <= 0 || portint > 65535 {
+			result = multierror.Append(result, errors.New("Port outside valid range"))
+		}
 	}
 	tlsPort = viper.GetString("tlsport")
-	if len(tlsPort) > 0{
+	if len(tlsPort) > 0 {
 		useTLS = true
-		tlsportint, err := strconv.Atoi(tlsPort);
+		tlsportint, err := strconv.Atoi(tlsPort)
 		if err != nil {
-			repErr := multierror.Prefix(err, "TLS Port not valid number") 
+			repErr := multierror.Prefix(err, "TLS Port not valid number")
 			result = multierror.Append(result, repErr)
 		}
 		if tlsportint <= 0 || tlsportint > 65535 {
@@ -79,20 +80,19 @@ func validateConfig() error{
 		if os.IsNotExist(err) {
 			result = multierror.Append(result, errors.New("TLS Key file not found"))
 		}
-
 	}
-		
-	if len(viper.GetString("username")) <1{
+
+	if len(viper.GetString("username")) < 1 {
 		log.Println("Warning: No username set.")
-	}else{
+	} else {
 		htAuthUser = viper.GetString("username")
 	}
-	if len(viper.GetString("password")) <1{
+	if len(viper.GetString("password")) < 1 {
 		log.Println("Warning: No password set.")
-	}else{
+	} else {
 		htAuthPass = viper.GetString("password")
 	}
-	
+
 	staticFilePath = viper.GetString("filepath")
 	if len(staticFilePath) < 1 {
 		result = multierror.Append(result, errors.New("FilePath not set"))
@@ -100,27 +100,24 @@ func validateConfig() error{
 	fi, err := os.Stat(staticFilePath)
 	if os.IsNotExist(err) {
 		result = multierror.Append(result, errors.New("FilePath doesn't exist"))
-	}else if fi.Mode(); !fi.IsDir(){
+	} else if fi.Mode(); !fi.IsDir() {
 		result = multierror.Append(result, errors.New("FilePath is not a valid directory"))
 	}
 
-
 	torrentsFilePath = viper.GetString("torrentsfilepath")
-	if len(torrentsFilePath) < 1{
+	if len(torrentsFilePath) < 1 {
 		log.Println("Warning: No torrent cache path set.")
-	}else{
+	} else {
 		fi, err := os.Stat(torrentsFilePath)
 		if os.IsNotExist(err) {
 			result = multierror.Append(result, errors.New("Torrents path doesn't exist"))
-		}else if fi.Mode(); !fi.IsDir(){
+		} else if fi.Mode(); !fi.IsDir() {
 			result = multierror.Append(result, errors.New("Torrents path is not a valid directory"))
 		}
 	}
 
-	
 	return result.ErrorOrNil()
 }
-
 
 func main() {
 	// Echo instance
@@ -134,53 +131,49 @@ func main() {
 	e.GET("/", hello)
 	gl := e.Group("/list")
 	gf := e.Group("/files")
-	if (len(htAuthUser) > 0 && len(htAuthPass) > 0){
+	if len(htAuthUser) > 0 && len(htAuthPass) > 0 {
 		gf.Use(middleware.BasicAuth(authMidHandler))
 		gl.Use(middleware.BasicAuth(authMidHandler))
 	}
 	gf.Static("/", staticFilePath)
 
-	if len(torrentsFilePath) > 0{
+	if len(torrentsFilePath) > 0 {
 		log.Println("using listing")
 		gl.GET("", listHandler)
 		gl.GET("/", listHandler)
 	}
 
-
 	// Start server
 	if useTLS {
-	go func() {
-		e.Logger.Fatal(e.StartTLS(":"+tlsPort, tlsCert, tlsKey))
+		go func() {
+			e.Logger.Fatal(e.StartTLS(":"+tlsPort, tlsCert, tlsKey))
 		}()
 	}
-	e.Logger.Fatal(e.Start(":"+mainPort))
+	e.Logger.Fatal(e.Start(":" + mainPort))
 }
 
 // Handler
 func hello(c echo.Context) error {
-  return c.String(http.StatusOK, "github.com/zvodd/webseeder")
+	return c.String(http.StatusOK, "github.com/zvodd/webseeder")
 }
-
 
 // list
 func listHandler(c echo.Context) error {
 
 	var files []string
-    err := filepath.Walk(torrentsFilePath, func(path string, info os.FileInfo, err error) error {
-		p,_ :=filepath.Rel(torrentsFilePath, path)
-		if p != "."{
+	err := filepath.Walk(torrentsFilePath, func(path string, info os.FileInfo, err error) error {
+		p, _ := filepath.Rel(torrentsFilePath, path)
+		if p != "." {
 			files = append(files, p)
 		}
-        return nil
-    })
-    if err != nil {
+		return nil
+	})
+	if err != nil {
 		c.String(http.StatusFailedDependency, "nope")
-    }
+	}
 	return c.JSON(http.StatusAccepted, files)
 
 }
-
-
 
 func authMidHandler(username, password string, c echo.Context) (bool, error) {
 	if username == htAuthUser && password == htAuthPass {
